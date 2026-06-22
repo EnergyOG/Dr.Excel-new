ccf858d61a5c02bce5e3430c021532fe919c1ad2
EnergyOG <dennispeprah34@gmail.com>
2026-06-22 22:32:35 +0000

    database refactored

--- Project_1/backend/src/config/database.js
+++ Project_1/backend/src/config/database.js
@@ -1,33 +1,27 @@
 import mongoose from "mongoose";
 import logger from "../utils/logger.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const mongoURI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
-    logger.log(`Successfully connected to database on ${conn.connection.host}`);
+
+    logger.info(`MongoDB connected: ${conn.connection.host}`);
 
     mongoose.connection.on("error", (err) => {
-      logger.error("MongoDB connection error:", err);
+      logger.error(`MongoDB connection error: ${err.message}`);
     });
 
-    mongoose.connection.on("disconnected", (err) => {
-      logger.log("MongoDB disconnected");
+    mongoose.connection.on("disconnected", () => {
+      logger.warn("MongoDB disconnected");
     });
-
-    mongoose.connection.on;
 
     process.on("SIGINT", async () => {
       await mongoose.connection.close();
-      logger.log("MongoDB connection closed due to app termination");
+      logger.info("MongoDB connection closed due to app termination");
       process.exit(0);
     });
   } catch (error) {
