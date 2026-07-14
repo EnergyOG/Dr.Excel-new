7f67bbef02b4b47ded4e6a16f1514955e8e9d2bd
EnergyOG <dennispeprah34@gmail.com>
2026-07-14 00:30:09 +0000

    Initial Terraform project

---
+++ Project_1/terraform/modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+    description = "This is the VPC CIDR block"
+    type = string
+}
+
+variable "azs" {
+  description = "List of availability zones in the region"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+    description = "List of private subnets in the VPC"
+    type = list(string)
+    }
+
+variable "public_subnets" {
+    description = "List of public subnets in the VPC"
+    type = list(string)
+}
